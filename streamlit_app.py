cb4ddbec3506bcf1e94533ca5bc437474b97efbb
jillkaria2709 <157040744+jillkaria2709@users.noreply.github.com>
2024-09-23 23:35:24 +0000

    eh

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,13 @@ import streamlit as st
 from streamlit import session_state as state
 
 # Define individual pages for homework 1 and homework 2
+hw4_page = st.Page("HW4.py", title="HW4")
 hw3_page = st.Page("HW3.py", title="HW3")
 hw2_page = st.Page("HW2.py", title="HW2")
 hw1_page = st.Page("HW1.py", title="HW1")
 
 # Initialize navigation with the pages
-pg = st.navigation([hw3_page,hw2_page,hw1_page])
+pg = st.navigation([hw4_page,hw3_page,hw2_page,hw1_page])
 
 # Set page configuration (optional but helps with page title and icon)
 st.set_page_config(page_title="Homework Manager", page_icon=":memo:")
