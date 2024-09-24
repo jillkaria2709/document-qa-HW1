d89127589f2ce821436161399849bd94c0d221f0
jillkaria2709 <157040744+jillkaria2709@users.noreply.github.com>
2024-09-24 00:40:38 +0000

    eh

--- HW4.py
+++ HW4.py
@@ -8,3 +8,22 @@ import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import chromadb
+
+chroma_client = chromadb.PersistentClient()
+
+collection = chroma_client.get_or_create_collection(name="My_Collection")
+
+collection.upsert(
+    documents=[
+        "This is a document about pineapples",
+        "This is a document about oranges"
+    ],
+    ids=["id1","id2"]
+)
+
+results = collection.query(
+    query_texts=["This is a query document about florida"],
+    n_results= 2
+)
+
+print(results)
